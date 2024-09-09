736b684856c6274ce1a46d2ebd53e4810f5bc9cc
justinavu <justinvu2008@gmail.com>
2024-09-09 10:15:34 -0700

    sync

--- Problem0.java
+++ Problem0.java
@@ -1,6 +1,6 @@
 public class Problem0
 {
-  public static void main(String[] args)
+    public static void main(String[] args)
   {
     int x = ((15 % 3) / 3) * (7 - 4) + 5;
     System.out.println(x);
